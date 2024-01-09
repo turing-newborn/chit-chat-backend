b69b8d299e2ff9b37b7bb9fc99356613220172de
Rishav Gupta <gupta.rishav512@gmail.com>
2024-01-09 20:53:57 +0530

    Initial Working Code Setup

---
+++ src/main/java/com/chitchat/ChitChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.chitchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChitChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChitChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chitchat/ChitChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChitChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

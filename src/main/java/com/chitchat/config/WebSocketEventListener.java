c2e34193ff7d7bf8cfc7e62336c2dcb4820fad96
Rishav Gupta <gupta.rishav512@gmail.com>
2024-01-21 13:34:10 +0530

    WebSocket and other config created

---
+++ src/main/java/com/chitchat/config/WebSocketEventListener.java
@@ -0,0 +1,35 @@
+package com.chitchat.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
+public class WebSocketEventListener {
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+        log.info("Received a new web socket connection...");
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectLister(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+//        String username = (String) headerAccessor.getSessionAttributes().get("username");
+//        if(username !=null) {
+//            log.info("User Disconnected:"+username);
+//            String chatMessage=username +" is Offline....";
+//            messagingTemplate.convertAndSend("/start/initial."+username, chatMessage);
+//        }
+    }
+}
